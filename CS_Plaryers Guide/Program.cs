16cbcb554763747ffaa5fcc44df2dcfa5a1563b7
Goodness <Goodness@DESKTOP-K9N9VLC>
2026-06-25 22:17:38 -0400

    Completed Simula's Test

--- CS_Plaryers Guide/Program.cs
+++ CS_Plaryers Guide/Program.cs
@@ -1,69 +1,123 @@
 ﻿
-int manticore = 10;
-int city = 15;
-int round = 1;
-bool play = true;
+//int manticore = 10;
+//int city = 15;
+//int round = 1;
+//bool play = true;
 
-Console.Write("Player 1 how far away from the city do you want to station the Manticore?");
-int station = Convert.ToInt32(Console.ReadLine());
-Console.Clear();
-Console.WriteLine("Player 2, it's your turn.");
+//Console.Write("Player 1 how far away from the city do you want to station the Manticore?");
+//int station = Convert.ToInt32(Console.ReadLine());
+//Console.Clear();
+//Console.WriteLine("Player 2, it's your turn.");
 
 
-while (play)
-{
+//while (play)
+//{
 
-    Console.WriteLine("-----------------------------------------------------------------------");
-    Console.WriteLine($"STATUS: Round {round} City: {city}/15 Manticore: {manticore}/10");
-    Console.WriteLine($"The cannon is expected to deal {Damage(round)} damage this round");
-    Console.Write("Enter desired cannon range: ");
-    int range  = Convert.ToInt32(Console.ReadLine());
-    if (range > station)
+//    Console.WriteLine("-----------------------------------------------------------------------");
+//    Console.WriteLine($"STATUS: Round {round} City: {city}/15 Manticore: {manticore}/10");
+//    Console.WriteLine($"The cannon is expected to deal {Damage(round)} damage this round");
+//    Console.Write("Enter desired cannon range: ");
+//    int range  = Convert.ToInt32(Console.ReadLine());
+//    if (range > station)
+//    {
+//        Console.WriteLine("That round OVERSHOT the target.");
+//    }
+//    else if (range == station)
+//    {
+//        manticore -= Damage(round);
+//        Console.WriteLine("That round was a DIRECT HIT.");
+//    }
+//    else if (range < station) {
+
+//        Console.WriteLine("That round FELL SHORT of the target");
+//    }
+//    Console.WriteLine("-------------------------------------------------------------------------");
+//    if (manticore <= 0) {
+//        Console.WriteLine("The Manticore has been destroyed! The Cirty of Consolas has been saved");
+//        play = false;
+//    }
+//    if (city <= 0) { 
+//           play = false; 
+//   }
+
+//    city--;
+//    round++;
+
+
+//}
+
+
+//int Damage(int roundNumber) {
+
+//    if (roundNumber % 3 == 0 && roundNumber % 5 == 0)
+//    {
+
+//        return 10;
+//    }
+//    else if (roundNumber % 3 == 0 || roundNumber % 5 == 0)
+//    {
+
+//        return 3;
+
+//    }
+//    else {
+//        return 1;
+//    }
+
+//}
+
+Status currentStatus = Status.locked;
+
+while (true) {
+
+    Console.Write($"the chest is {currentStatus}, what would you like to do? " );
+    string action  = Console.ReadLine();
+
+    if (currentStatus == Status.locked && action == "unlock")
     {
-        Console.WriteLine("That round OVERSHOT the target.");
+
+        currentStatus = Status.closed;
     }
-    else if (range == station)
-    {
-        manticore -= Damage(round);
-        Console.WriteLine("That round was a DIRECT HIT.");
-    }
-    else if (range < station) {
-
-        Console.WriteLine("That round FELL SHORT of the target");
-    }
-    Console.WriteLine("-------------------------------------------------------------------------");
-    if (manticore <= 0) {
-        Console.WriteLine("The Manticore has been destroyed! The Cirty of Consolas has been saved");
-        play = false;
-    }
-    if (city <= 0) { 
-           play = false; 
-   }
-
-    city--;
-    round++;
-
-
-}
-
-
-int Damage(int roundNumber) {
-
-    if (roundNumber % 3 == 0 && roundNumber % 5 == 0)
+    else if (currentStatus == Status.closed)
     {
 
-        return 10;
+        if (action == "open")
+        {
+
+            currentStatus = Status.open;
+        }
+        else if (action == "lock")
+        {
+            currentStatus = Status.locked;
+        }
+        else
+        {
+
+            Console.WriteLine("Invalid Action");
+        }
     }
-    else if (roundNumber % 3 == 0 || roundNumber % 5 == 0)
+    else if (currentStatus == Status.open && action == "close")
     {
 
-        return 3;
+        currentStatus = Status.closed;
 
     }
+
     else {
-        return 1;
-    }
+
+        Console.WriteLine("Invalid action");
+            }
 
 }
 
+
+enum Status { 
+
+
+
+open, 
+closed, 
+locked
+
+}
 
